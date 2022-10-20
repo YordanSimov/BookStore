cdb7486c6cb9e3f26ca31898ada783df1ad4f99f
Yordan Simov <y.simov@draftkings.com>
2022-10-20 14:52:52 +0300

    Added HttpClient

--- ProjectDK/ProjectDK.BL/Dataflow/PurchaseDataflow.cs
+++ ProjectDK/ProjectDK.BL/Dataflow/PurchaseDataflow.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson.IO;
 using ProjectDK.BL.Kafka;
 using ProjectDK.DL.Interfaces;
 using ProjectDK.Models.Configurations;
 using ProjectDK.Models.Models;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks.Dataflow;
 
 namespace ProjectDK.BL.Dataflow
@@ -23,9 +27,25 @@ namespace ProjectDK.BL.Dataflow
             this.bookRepository = bookRepository;
             this.kafkaConsumerService = new KafkaConsumerService<Guid, Purchase>(kafkaConsumerSettings, HandlePurchase);
             this.kafkaConsumerSettings = kafkaConsumerSettings;
-            updateBlock = new TransformBlock<Purchase,IEnumerable<Book>>(async x =>
+            updateBlock = new TransformBlock<Purchase, IEnumerable<Book>>(async purchase =>
             {
-               var result = await UpdateQuantity(x);
+                var additionalInfoText = new List<string>();
+                HttpClient client = new HttpClient();
+
+                    client.BaseAddress = new Uri("https://localhost:49157/AdditionalInfo/GetAdditionalInfoById?id=");
+                foreach (var book in purchase.Books)
+                {
+                    var response = await client.GetAsync($"{client.BaseAddress}{book.AuthorId}");
+                    response.EnsureSuccessStatusCode();
+
+                    var additionalInfo = await response.Content.ReadAsStringAsync();
+
+                    additionalInfoText.Add(additionalInfo);
+                }
+                purchase.AdditionalInfo = additionalInfoText;
+              
+                var result = await UpdateQuantity(purchase);
+
                 return result;
             });
             addBlock = new ActionBlock<IEnumerable<Book>>(async result =>
